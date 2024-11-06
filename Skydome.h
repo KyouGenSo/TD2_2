24ae8c0ed4b9c337c7769825d4082e6baae684ba
takuma <k023g0015@g.neec.ac.jp>
2024-11-06 15:13:04 +0900

    skydome実装

---
+++ Skydome.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <d3d12.h>
+#include<wrl.h>
+#include <string>
+#include <vector>
+#include "vector2.h"
+#include "vector3.h"
+#include "vector4.h"
+#include "Mat4x4Func.h"
+#include <memory>
+
+class Object3d;
+
+class Skydome
+{
+
+public:
+	void Initialize();
+	void Update();
+	void Draw();
+
+private:
+	std::unique_ptr<Object3d> object3d_ = nullptr;
+
+	Transform transform_;
+
+};
+
