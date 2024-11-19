0ec845013525c167300af8bc9c093efff202f0fa
takuma <k023g0015@g.neec.ac.jp>
2024-11-19 14:08:38 +0900

    範囲が広く光が弱いライト設定、実装

--- Player.cpp
+++ Player.cpp
@@ -43,11 +43,11 @@ void Player::Initialize(Boss* boss) {
 	wideWeakLight_ = {
 		{ transform_.translate.x, transform_.translate.y + 2.0f, transform_.translate.z }, // lightPos
 		boss->GetTransform().translate - Vector3(transform_.translate.x, transform_.translate.y + 2.0f, transform_.translate.z), // lightDir
-		{ 0.5f, 0.5f, 0.5f, 1.0f }, // lightColor
-		1.0f, // 光の強さ
+		{ 1.0f, 1.0f, 1.0f, 1.0f }, // lightColor
+		5.0f, // 光の強さ
 		20.0f, // ライト範囲
 		1.0f, // 光減衰
-		std::cos(std::numbers::pi_v<float> / -10.0f), // ライトスポット角度
+		std::cos(std::numbers::pi_v<float> / 5.0f), // ライトスポット角度
 		true // isSpotLightフラグ
 	};
 
